e1b8c5e85e9cca949788350340e84e1c437259fc
rizk <rizkdotai@gmail.com>
2024-02-17 18:48:59 +0200

    add custom class styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+      <h1>Hello, world!</h1>
+    </div>
   )
 }
 
 export default App
+
