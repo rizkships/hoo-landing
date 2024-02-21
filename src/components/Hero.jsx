ca1a8b883e9613df448170838bd65292edb2a231
rizk <rizkdotai@gmail.com>
2024-02-21 17:16:17 +0200

    style hero container

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from '../style'
+import { discount, robot } from '../assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div>
+
+      </div>
+    </section>
   )
 }
 
