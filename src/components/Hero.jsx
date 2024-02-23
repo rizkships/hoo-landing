795eed9303f38b7ccc96f10985a94d46720d5934
rizk <rizkdotai@gmail.com>
2024-02-23 08:40:05 +0200

    style hero header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import styles from '../style'
 import { discount, robot } from '../assets'
+import GetStarted from "./GetStarted";
+
 
 const Hero = () => {
   return (
+    /* create discount banner */
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
@@ -14,6 +17,17 @@ const Hero = () => {
           </p>
         </div>
         </div>
+      
+
+      <div className="flex flex-row justify-between items-center w-full">
+      <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+        The Next <br className="sm:block hidden" />{" "}
+        <span className="text-gradient">Generation</span>{" "}
+      </h1>
+      <div className="ss:flex hidden md:mr-4 mr-0">
+        <GetStarted />
+      </div>
+      </div>
       </section>
   )
 }
