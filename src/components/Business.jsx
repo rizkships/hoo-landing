507334dd363832e7aeedb270432f9bdae38ec0c4
rizk <rizkdotai@gmail.com>
2024-02-24 15:49:04 +0200

    create business section component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -17,3 +17,29 @@ const FeatureCard = ({ icon, title, content, index }) => (
     </div>
   </div>
 );
+
+const Business = () =>  (
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        You do the business, <br className="sm:block hidden" /> we’ll handle
+        the money.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index} />
+      ))}
+    </div>
+  </section>
+);
+
+export default Business;
